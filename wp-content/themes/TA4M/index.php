5a84b5445575ab7b9cdeaf8b3ce4f081c2389db0
jallend1 <jallend1@gmail.com>
2021-11-11 15:30:04 -0800

    Creates basic blog list layout

--- wp-content/themes/TA4M/index.php
+++ wp-content/themes/TA4M/index.php
@@ -1,5 +1,28 @@
 <?php get_header(); ?>
 
-hello i am a blog
+<?php 
+    while(have_posts()){
+        the_post();?>
+        <article class="post">
+            <div class="post-wrapper">
+                <div class="post-container">    
+                    <div class="post-details">    
+                        <a href="<?php the_permalink(); ?>">
+                            <h3><?php the_title(); ?></h3>
+                        </a>
+                        <div>
+                            <p><?php the_excerpt(); ?>
+                            <p>
+                                <a href="<?php echo the_permalink(); ?>">Continue reading &raquo;</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="post-image">
+                        <?php echo get_the_post_thumbnail(); ?>    
+                    </div>
+                </div>
+            </div>
+        </article>
+    <?php } ?>
 
 <?php get_footer(); ?>
