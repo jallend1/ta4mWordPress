a414092303979af0c5156b52225de81581739737
jallend1 <jallend1@gmail.com>
2021-11-13 13:33:24 -0800

    Updates .gitignore to start tracking previously missed files

---
+++ wp-content/themes/TA4M/page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+    <div class="container">
+        <aside class="sidebar">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/4MicsLogo.png">
+            <div>
+                <p>There Are Four Mics is a weekly podcast dedicated to group discussion of Star Trek.</p>
+                <p>Join your hosts as they make their way through the Star Trek franchise episode by episode, movie by movie, in stardate order. Grab a beer and join us as we talk about the episodes we love and the occasional episode we love to hate.</p>
+            </div>
+        </aside>
+        <main>
+            <?php while(have_posts()){
+                the_post(); ?>
+                <h2><?php the_title(); ?></h2>
+                <div>
+                    <?php the_content(); ?>
+                </div>
+            <?php } ?>
+        </main>
+    </div>
+<?php get_footer(); ?>
