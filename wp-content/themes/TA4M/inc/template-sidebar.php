8dd13b7f0c9c2a6b7e12aff8a3f8ab50afbbaef9
jallend1 <jallend1@gmail.com>
2021-11-13 13:37:07 -0800

    Converts repeated sidebar code into template part

--- wp-content/themes/TA4M/inc/template-sidebar.php
+++ wp-content/themes/TA4M/inc/template-sidebar.php
@@ -1,1 +1,7 @@
-<?php ?>
+<aside class="sidebar">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/4MicsLogo.png">
+    <div>
+        <p>There Are Four Mics is a weekly podcast dedicated to group discussion of Star Trek.</p>
+        <p>Join your hosts as they make their way through the Star Trek franchise episode by episode, movie by movie, in stardate order. Grab a beer and join us as we talk about the episodes we love and the occasional episode we love to hate.</p>
+    </div>
+</aside>
