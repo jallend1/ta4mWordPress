5aeacedafb9f76c1a815a3877383a7dbc237125f
jallend1 <jallend1@gmail.com>
2021-11-14 12:04:43 -0800

    Creates archive page template

---
+++ wp-content/themes/TA4M/archive.php
@@ -0,0 +1,46 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <?php get_template_part('./inc/template-sidebar'); ?>
+    <main class="post-index">
+        <div class='archive-head'>
+            <h2><?php the_archive_title(); ?></h2>
+            <div><?php the_archive_description(); ?></div>
+        </div>
+        <div>
+        <?php while(have_posts()){
+            the_post(); ?>
+            <article class="post">
+                <div class="post-wrapper">
+                    <div class="post-container">    
+                        <div class="post-details">    
+                            <a href="<?php the_permalink(); ?>">
+                                <h3><?php the_title(); ?></h3>
+                            </a>
+                            <div>
+                                <p>
+                                    <?php
+                                        if(has_excerpt()){
+                                            echo get_the_excerpt();
+                                        }
+                                        else{
+                                            echo wp_trim_words(get_the_content(), 100); 
+                                        }
+                                    ?>
+                                <p>
+                                    <a href="<?php the_permalink(); ?>">Continue reading &raquo;</a>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="post-image">
+                            <?php echo get_the_post_thumbnail(); ?>    
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <?php } ?>
+        </div>
+    </main>
+</div>
+
+<?php get_footer(); ?>
--- wp-content/themes/TA4M/single.php
+++ wp-content/themes/TA4M/single.php
@@ -12,6 +12,7 @@
                             <h3><?php the_title(); ?> </h3>
                             <p><?php the_content(); ?> </p>
                         </div>
+                        <?php echo the_tags(); ?>
                     </div>
                 </div>
                 <div>
