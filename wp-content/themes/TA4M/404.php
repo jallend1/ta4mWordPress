7aec4561032d60ddd4d33c6bf1e9862e3b9cc6c8
jallend1 <jallend1@gmail.com>
2021-11-14 12:27:15 -0800

    Creates basic 404 page

---
+++ wp-content/themes/TA4M/404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<div class="container">
+    <div>
+        <?php get_template_part('./inc/template-sidebar'); ?>
+    </div>
+    <div>
+        <h2>Oh no! We can't find that page!</h2>
+    </div>
+</div>
+
+<?php get_footer(); ?>
