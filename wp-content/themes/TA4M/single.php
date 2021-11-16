f374c0016934853050cada8a65562eba7186658f
jallend1 <jallend1@gmail.com>
2021-11-15 18:31:44 -0800

    Begins adding post meta-data into single page posts

--- wp-content/themes/TA4M/single.php
+++ wp-content/themes/TA4M/single.php
@@ -6,31 +6,38 @@
         while(have_posts()){
             the_post();?>
             <article class="post">
-                <div class="post-wrapper">
-                    <div class="post-container">    
+                <div>
+                    <div>    
                         <div class="post-details">    
                             <h3><?php the_title(); ?> </h3>
                             <p><?php the_content(); ?> </p>
                         </div>
-                        <?php echo the_tags(); ?>
                     </div>
                 </div>
-                <div>
-                    <?php 
-                        if(comments_open() || get_comments_number()) : comments_template(); 
-                        endif;
-                    ?>
-                </div>
-                <!-- Previous / Next Post Navigation -->
-                <?php
-                    the_post_navigation(array(
-                        'next_text' => '<span class="meta-nav" aria-hidden="true">Next article: </span> <span class="post-title">%title</span>',
-                        'prev_text' => '<span class="meta-nav" aria-hidden="true">Previous article: </span><span class="post-title">%title</span>',
-                    ));
-                    ?>
             </article>
-        <?php } ?>
-    </main>
+            <?php } ?>
+        </main>
+        <div class="post-meta">
+            <div class="post-tags">
+                <?php echo the_tags(); ?>
+            </div>
+            <div>
+                Posted by: <?php the_author(); ?> on <?php the_time(); ?>
+            </div>
+            <!-- Previous / Next Post Navigation -->
+            <?php
+                the_post_navigation(array(
+                    'next_text' => '<span class="meta-nav" aria-hidden="true">Next article: </span> <span class="post-title">%title</span>',
+                    'prev_text' => '<span class="meta-nav" aria-hidden="true">Previous article: </span><span class="post-title">%title</span>',
+                ));
+            ?>
+            <div>
+                <?php 
+                    if(comments_open() || get_comments_number()) : comments_template(); 
+                    endif;
+                ?>
+            </div>
+        </div>
 </div>
 
 <?php get_footer(); ?>
