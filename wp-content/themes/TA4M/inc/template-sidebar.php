cfe02df069fafa50f71bdf1f6ab5bb8a06bdeef5
jallend1 <jallend1@gmail.com>
2021-11-15 18:07:19 -0800

    Expands sidebar to include Disco Night and additional styles

--- wp-content/themes/TA4M/inc/template-sidebar.php
+++ wp-content/themes/TA4M/inc/template-sidebar.php
@@ -1,7 +1,14 @@
 <aside class="sidebar">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/4MicsLogo.png">
-    <div>
-        <p>There Are Four Mics is a weekly podcast dedicated to group discussion of Star Trek.</p>
-        <p>Join your hosts as they make their way through the Star Trek franchise episode by episode, movie by movie, in stardate order. Grab a beer and join us as we talk about the episodes we love and the occasional episode we love to hate.</p>
+    <div>    
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/4MicsLogo.png">
+        <p>
+            There Are Four Mics is a Star Trek podcast where the group makes their way through the series, one episode at a time in Stardate order. Grab a beer and join us as we talk about the episodes we love and the occasional episode we love to hate. 
+        </p>
     </div>
+    <div class="itunes">
+        <a href="https://itunes.apple.com/us/podcast/there-are-four-mics-a-star-trek-podcast/id1167277814?mt=2&at=1010lxKy">Rate us on iTunes</a>
+    </div>
+    <div class="disco">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/DiscoNight.png">
+        <p>Like us? Or, more likely, like Star Trek Discovery? Check out our Star Trek Discovery podcast: Disco Night</p>
 </aside>
