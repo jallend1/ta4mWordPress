b9431d7b2bc9a8af9172cb4f3ddb751d541bcade
jallend1 <jallend1@gmail.com>
2021-11-17 18:49:26 -0800

    Begins rolling out some custom fields

--- wp-content/themes/TA4M/page.php
+++ wp-content/themes/TA4M/page.php
@@ -1,12 +1,15 @@
 <?php get_header(); ?>
 <div class="container">
     <?php get_template_part('./inc/template-sidebar'); ?>
+
     <main>
         <?php while(have_posts()){
             the_post(); ?>
-            <h2><?php the_title(); ?></h2>
+            
+            <h2><?php the_field('title'); ?></h2>
             <div>
-                <?php the_content(); ?>
+                <?php the_field('blurb'); ?>
+                <!-- <?php the_content(); ?> -->
             </div>
         <?php } ?>
     </main>
