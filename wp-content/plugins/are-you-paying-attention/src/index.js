d4081ca472142e1ccc53330dce0d1dc4e96f10d6
jallend1 <jallend1@gmail.com>
2021-11-29 12:02:08 -0800

    Adds additional styling to buttons and input fields on editor

--- wp-content/plugins/are-you-paying-attention/src/index.js
+++ wp-content/plugins/are-you-paying-attention/src/index.js
@@ -1,5 +1,12 @@
 import './index.scss';
-import { TextControl } from '@wordpress/components';
+import {
+  TextControl,
+  Flex,
+  FlexBlock,
+  FlexItem,
+  Button,
+  Icon
+} from '@wordpress/components';
 
 wp.blocks.registerBlockType('ourplugin/are-you-paying-attention', {
   title: 'Are You Paying Attention?',
@@ -14,7 +21,24 @@ wp.blocks.registerBlockType('ourplugin/are-you-paying-attention', {
 function EditComponent(props) {
   return (
     <div className='paying-attention-edit-block'>
-      <TextControl label='Question: ' />
+      <TextControl label='Question: ' style={{ fontSize: '20px' }} />
+      <p style={{ fontSize: '13px', margin: '20px 0 8px 0' }}>Answers:</p>
+      <Flex>
+        <FlexBlock>
+          <TextControl />
+        </FlexBlock>
+        <FlexItem>
+          <Button>
+            <Icon icon='star-empty' className='mark-as-correct' />
+          </Button>
+        </FlexItem>
+        <FlexItem>
+          <Button className='attention-delete' isLink>
+            Delete
+          </Button>
+        </FlexItem>
+      </Flex>
+      <Button isPrimary>Add another answer</Button>
     </div>
   );
 }
