ef075cdd6ce6887bde001e7e5ce67744d2a70878
jallend1 <jallend1@gmail.com>
2021-11-29 11:31:19 -0800

    Adds styling to question component in WP Editor

--- wp-content/plugins/are-you-paying-attention/src/index.js
+++ wp-content/plugins/are-you-paying-attention/src/index.js
@@ -1,16 +1,20 @@
+import './index.scss';
 import { TextControl } from '@wordpress/components';
 
 wp.blocks.registerBlockType('ourplugin/are-you-paying-attention', {
   title: 'Are You Paying Attention?',
   icon: 'smiley',
   category: 'common',
-  edit: EditComponent
+  edit: EditComponent,
+  save: function (props) {
+    return null;
+  }
 });
 
 function EditComponent(props) {
   return (
-    <div>
-      <TextControl />
+    <div className='paying-attention-edit-block'>
+      <TextControl label='Question: ' />
     </div>
   );
 }
