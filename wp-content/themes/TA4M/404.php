ba5a56ba085539c0f3972926db705a8e50619567
jallend1 <jallend1@gmail.com>
2021-12-06 14:49:21 -0800

    Adds preliminary 404 page

--- wp-content/themes/TA4M/404.php
+++ wp-content/themes/TA4M/404.php
@@ -3,9 +3,22 @@
     <div>
         <?php get_template_part('./inc/template-sidebar'); ?>
     </div>
-    <div>
-        <h2>Definitely going to put an LCARS menu (white and red) here saying 404</h2>
-    </div>
+    <main>
+        <header class="lcars-404">
+            <div class='red-bar x2'></div>
+            <h2>404</h2>
+            <div class='red-bar x7'></div>
+        </header>
+        <p class="error-text">Page Not Found.</p>
+        <p>Let's go back <a href="<?php echo site_url() . "/home" ?>"">Home</a>.</p>
+        <footer class="lcars-404">
+            <div class='red-bar x7'></div>
+            <h2>404</h2>
+            <div class='red-bar x2'></div>
+        </footer>
+    </main>
 </div>
 
 <?php get_footer(); ?>
+
+body > div:nth-child(3) > main > p
